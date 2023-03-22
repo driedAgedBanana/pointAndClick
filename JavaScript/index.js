30cb06b6ec2fc136cc34aff91e0710b46c85b5e9
Wyzard12 <b.cao@sintlucasedu.nl>
2023-03-22 16:37:39 +0100

    Change!

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,5 +1,6 @@
 document.getElementById("mainTitle").innerText = "Point and click Advent";
 
+const offsetChar = 10;
 const gameWindow = document.getElementById("gameWindow");
 const mainChar = document.getElementById("mainChar");
 
@@ -7,7 +8,19 @@ gameWindow.onclick = function(e) {
     var rect = gameWindow.getBoundingClientRect();
     var x = e.clientX - rect.left; //X position with the elements
     var y = e.clientY - rect.top; //Y position for the elemts
-    mainChar.style.left = x+"px";
-    mainChar.style.top = y+"px";
-    console.log(x + " " + y);
+    mainChar.style.left = x - offsetChar+"px";
+    mainChar.style.top = y - offsetChar + "px";
+
+    console.log(e.target.id);
+    switch(e.target.id) {
+        case "funeral1":
+            console.log("Who rest here?..")
+         break;
+        case "grass1":
+            console.log("Damn the dev didn't even finnish this properly!")
+            break;
+        default:
+            console.log("...")
+        break;
+    }
 }
