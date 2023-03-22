d74a4c8998ba4475e95f73c14e3d76b46d3337be
Wyzard12 <b.cao@sintlucasedu.nl>
2023-03-22 16:00:28 +0100

    Y

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -3,9 +3,11 @@ document.getElementById("mainTitle").innerText = "Point and click Advent";
 const gameWindow = document.getElementById("gameWindow");
 const mainChar = document.getElementById("mainChar");
 
-gameWindow.onclick = function() {
+gameWindow.onclick = function(e) {
     var rect = gameWindow.getBoundingClientRect();
     var x = e.clientX - rect.left; //X position with the elements
     var y = e.clientY - rect.top; //Y position for the elemts
+    mainChar.style.left = x+"px";
+    mainChar.style.top = y+"px";
     console.log(x + " " + y);
 }
