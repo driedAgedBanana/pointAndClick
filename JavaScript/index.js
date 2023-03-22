fe7b9e04732c9e0b2751bc80e5320a209d74e925
Wyzard12 <b.cao@sintlucasedu.nl>
2023-03-22 15:55:59 +0100

    bread

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,1 +1,11 @@
 document.getElementById("mainTitle").innerText = "Point and click Advent";
+
+const gameWindow = document.getElementById("gameWindow");
+const mainChar = document.getElementById("mainChar");
+
+gameWindow.onclick = function() {
+    var rect = gameWindow.getBoundingClientRect();
+    var x = e.clientX - rect.left; //X position with the elements
+    var y = e.clientY - rect.top; //Y position for the elemts
+    console.log(x + " " + y);
+}
