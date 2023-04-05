194b5444a35fe22f1a55917deff6c72989c51334
Wyzard12 <b.cao@sintlucasedu.nl>
2023-04-05 16:56:32 +0200

    the

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,41 +1,39 @@
-document.getElementById("mainTitle").innerText = "Point and click Advent";
-
-const offsetChar = 10;
+document.getElementById("mainTitle").innerText = "Point and Click adventure";
+const offsetCharacter = 16;
+const sec = 1000;
 const gameWindow = document.getElementById("gameWindow");
 const mainChar = document.getElementById("mainChar");
 
 const mainCharSpeech = document.getElementById("mainCharSpeech");
 
-gameWindow.onclick = function(e) {
-    if(mainCharSpeech.style.opacity == 0) {
-     var rect = gameWindow.getBoundingClientRect();
-     var x = e.clientX - rect.left; //X position with the elements
-     var y = e.clientY - rect.top; //Y position for the elemts
-     mainChar.style.left = x - offsetChar+"px";
-     mainChar.style.top = y - offsetChar + "px";
+gameWindow.onclick = function (e) {
+    if (mainCharSpeech.style.opacity == 0) {
+        var rect = gameWindow.getBoundingClientRect();
+        var x = e.clientX - rect.left; //x position within the element.
+        var y = e.clientY - rect.top;  //y position within the element.
 
-     console.log(e.target.id);
-     switch(e.target.id) {
-        case "funeral1":
-            showMessage("Who rest here?..");
-            console.log("Who rest here?..")
-         break;
-        case "grass1":
-            showMessage("Damn the dev didn't even finnish this properly!");
-            console.log("Damn the dev didn't even finnish this properly!");
-            break;
-        default:
-            hideMessage()
-            console.log("...");
-        break;
-     }
+        mainChar.style.left = x - offsetCharacter + "px";
+        mainChar.style.top = y - offsetCharacter + "px";
+
+        console.log(e.target.id);
+        switch (e.target.id) {
+            case "funeral1":
+                showMessage("Who rest here?..");
+                break;
+            case "door2":
+                showMessage("Nobody is here... what should I do?");
+                break;
+            default:
+                hideMessage();
+                break;
+        }
     }
 }
 
 function showMessage(message) {
     mainCharSpeech.innerHTML = message;
     mainCharSpeech.style.opacity = 1;
-    setTimeout(hideMessage, 4000);
+    setTimeout(hideMessage, 4 * sec);
 }
 
 function hideMessage() {
